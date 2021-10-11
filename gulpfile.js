35484f1ddab2fdfee068700ed1aabae2f209a5e1
Roman Matsypula <ukrmatsypula@gmail.com>
2021-10-11 23:14:21 +0300

    added css task with watch css

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,8 @@ let { src, dest } = require("gulp"),
   gulp = require("gulp"),
   browsersync = require("browser-sync").create(),
   fileinclude = require("gulp-file-include"),
-  del = require("del");
+  del = require("del"),
+  scss = require('gulp-sass')(require('sass'));
 
 function browserSync(params) {
   browsersync.init({
@@ -51,18 +52,31 @@ function html() {
     .pipe(browsersync.stream());
 }
 
+function css() {
+  return src(path.src.css)
+    .pipe(
+      scss({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream());
+}
+
 function watchFiles() {
   gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
 }
 
 function clean() {
   return del(path.clean);
 }
 
-let build = gulp.series(clean, html);
+let build = gulp.series(clean, gulp.parallel(html, css));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
 exports.html = html;
+exports.css = css;
 exports.build = build;
 exports.watch = watch;
 exports.default = watch;
